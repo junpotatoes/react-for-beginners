bbebe03164b346b65a7616922c917e61d8b58a24
최준호 <103482590+junpotatous@users.noreply.github.com>
2022-08-21 23:58:17 +0900

    8.21 TIL

--- src/App.js
+++ src/App.js
@@ -1,33 +1,22 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("")
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([])
   useEffect(() => {
-    console.log("I run only once.");
-    }, []);
-  useEffect(() => {
-    console.log("I run when 'keyword' changes");
-  }, [keyword]);
-  useEffect(() => {
-    console.log("I run when 'counter' changes");
-  }, [counter]);
-  useEffect(() => {
-    console.log("I run when keyword & counter change")  
-  }, [keyword, counter]);
-  return (
-    <div>
-      <input 
-      value={keyword} 
-      onChange={onChange} 
-      type="text" 
-      placeholder="Search here..."/>
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
-    </div>
-  );
+    fetch("https://api.coinpaprika.com/v1/tickers").then(response => response.json()
+    ).then(json => {
+      setCoins(json);
+      setLoading(false);
+    });
+  }, [])
+  return <div>
+    <h1>The Coins!{loading ? "" : `(${coins.length})`}</h1>
+    {loading ? <strong>Loading...</strong> : <select>
+      {coins.map((coin) => <option>{coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD</option>)}
+    </select>}
+    
+  </div>
 }
-
+ 
 export default App;
